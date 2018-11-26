112e8f4540838101b6f789645554c836af39aa82
Renaud Trezieres <renaud.trezieres@capgemini.com>
2018-11-26 14:07:53 +0100

    integration slickUtils

---
+++ src/main/java/ui/TextArea.java
@@ -0,0 +1,142 @@
+package ui;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
+
+import org.newdawn.slick.Font;
+
+import ui.align.VerticalAlignement;
+import ui.formater.TextFormater;
+
+/**
+ * Code sous licence GPLv3 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author <b>Shionn</b>, shionn@gmail.com <i>http://shionn.org</i><br>
+ *         GCS d- s+:+ a+ C++ UL/M P L+ E--- W++ N K- w-- M+ t+ 5 X R+ !tv b+ D+ G- e+++ h+ r- y+
+ */
+public class TextArea {
+	private int x;
+	private int y;
+	private int width;
+	private int height;
+	private List<TextAreaLine> lines = new ArrayList<>();
+	private TextFormater formater = new TextFormater();
+	private boolean bottomUp;
+	private Font defaultFont;
+
+	public TextArea(int x, int y, int width, int height) {
+		this.x = x;
+		this.y = y;
+		this.width = width;
+		this.height = height;
+	}
+
+	public void render() {
+		int y = getStartLine();
+		Iterator<TextAreaLine> lines = this.lines.iterator();
+		while (!isLastLine(y) && lines.hasNext()) {
+			y = render(lines.next(), y);
+		}
+	}
+
+	private int getStartLine() {
+		int firstFontSize = lines.size() > 0 ? lines.get(0).getFont().getLineHeight() : 0;
+		return bottomUp ? y + height - firstFontSize : y;
+	}
+
+	private int render(TextAreaLine line, int starty) {
+		int y = starty;
+		Iterator<String> paragraphs = formater.splitParagraph(line.getText()).iterator();
+		while (!isLastLine(y) && paragraphs.hasNext()) {
+			y = render(line, paragraphs.next(), y);
+		}
+		return y;
+	}
+
+	private int render(TextAreaLine line, String paragraph, int starty) {
+		int y = starty;
+		Iterator<String> texts = getLines(line.getFont(), paragraph).iterator();
+		while (!isLastLine(y) && texts.hasNext()) {
+			renderLine(texts.next(), y, line.getFont(), line.getAlign());
+			y = getNextLineY(line.getFont(), y);
+		}
+		return y;
+	}
+
+	private boolean isLastLine(int y) {
+		return (bottomUp && y < this.y) || (!bottomUp && y >= this.getStartLine() + height);
+	}
+
+	private int getNextLineY(Font font, int y) {
+		return y + (bottomUp ? -font.getLineHeight() : font.getLineHeight());
+	}
+
+	private List<String> getLines(Font font, String p) {
+		List<String> lines = formater.splitLine(p, font, width);
+		if (bottomUp) {
+			Collections.reverse(lines);
+		}
+		return lines;
+	}
+
+	private void renderLine(String line, int liney, Font font, VerticalAlignement alignement) {
+		switch (alignement) {
+		case CENTER:
+			font.drawString(x + width / 2 - font.getWidth(line) / 2, liney, line);
+			break;
+		case RIGHT:
+			font.drawString(x + width - font.getWidth(line), liney, line);
+			break;
+		case LEFT:
+		default:
+			font.drawString(x, liney, line);
+			break;
+		}
+	}
+
+	public void clear() {
+		this.lines.clear();
+	}
+
+	public void addText(String text, VerticalAlignement align) {
+		if (defaultFont == null)
+			throw new IllegalStateException("Define default font whith setDefaultFont");
+		this.addText(text, defaultFont, align);
+	}
+	public void addText(String text, Font font, VerticalAlignement align) {
+		this.lines.add(new TextAreaLine(text, font, align));
+	}
+
+	public void addFirstText(String text, VerticalAlignement align) {
+		if (defaultFont == null)
+			throw new IllegalStateException("Define default font whith setDefaultFont");
+		this.addFirstText(text, defaultFont, align);
+	}
+
+	public void addFirstText(String text, Font font, VerticalAlignement align) {
+		this.lines.add(0, new TextAreaLine(text, font, align));
+	}
+
+	public void setBottomUp(boolean bottomUp) {
+		this.bottomUp = bottomUp;
+	}
+
+	public int count() {
+		return lines.size();
+	}
+
+	public boolean getBottomUp() {
+		return bottomUp;
+	}
+
+	public void keepFirst(int size) {
+		this.lines = lines.subList(0, Math.min(lines.size(), size));
+	}
+
+	public void setDefaultFont(Font defaultFont) {
+		this.defaultFont = defaultFont;
+	}
+
+}
---
+++ src/main/java/ui/formater/TextFormater.java
@@ -0,0 +1,51 @@
+package ui.formater;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.newdawn.slick.Font;
+
+/**
+ * Code sous licence GPLv3 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author <b>Shionn</b>, shionn@gmail.com <i>http://shionn.org</i><br>
+ *         GCS d- s+:+ a+ C++ UL/M P L+ E--- W++ N K- w-- M+ t+ 5 X R+ !tv b+ D+ G- e+++ h+ r- y+
+ */
+public class TextFormater {
+
+	private static final Pattern END_LINE = Pattern.compile("\\n");
+	private static final Pattern SEPARATOR = Pattern.compile(" ");
+
+	public List<String> splitParagraph(String text) {
+		return Arrays.asList(END_LINE.split(text));
+	}
+
+	public List<String> splitLine(String paragraph, Font font, int length) {
+		List<String> lines = new ArrayList<String>();
+		do {
+			String line = getLine(paragraph, font, length);
+			lines.add(line);
+			paragraph = paragraph.substring(line.length());
+		} while (paragraph.length() > 0);
+		return lines;
+	}
+
+	private String getLine(String text, Font font, int length) {
+		if (font.getWidth(text) <= length)
+			return text;
+		return text.substring(0, getSeparatorIndex(text, font, length));
+	}
+
+	private int getSeparatorIndex(String text, Font font, int length) {
+		Matcher m = SEPARATOR.matcher(text);
+		int index = text.length();
+		while (m.find() && font.getWidth(text.substring(0, m.end())) < length) {
+			index = m.end();
+		}
+		return index;
+	}
+
+}
